bcb83f098b27d14635cd1975fd0d9d65d61baf75
Inkhermit <lisongwei200110@gmail.com>
2024-10-12 23:04:58 -0500

    Complete Project Updated
    First time upload

---
+++ src/main/java/com/laioffer/twitch/model/DuplicateFavoriteException.java
@@ -0,0 +1,3 @@
+package com.laioffer.twitch.model;
+
+public class DuplicateFavoriteException extends Exception { }
---
+++ src/test/java/com/laioffer/twictch/TwictchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laioffer.twictch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwictchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
